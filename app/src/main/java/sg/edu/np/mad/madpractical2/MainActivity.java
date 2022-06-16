4b932fb9a4f915cba8c4768ff9c3b0dd046f75ee
Ryan Lim <s10192609@connect.np.edu.sg>
2022-06-16 18:55:43 +0800

    Completed Practical and Challenge

---
+++ app/src/main/java/sg/edu/np/mad/madpractical2/MainActivity.java
@@ -0,0 +1,59 @@
+package sg.edu.np.mad.madpractical2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    private String TAG = "Main Activity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.v(TAG, "On Create!");
+        Button followButton = findViewById(R.id.followButton);
+        Button messageButton = findViewById(R.id.messageButton);
+        TextView nameText = findViewById(R.id.nameText);
+        TextView descriptionText = findViewById(R.id.descriptionText);
+        ImageView imageView = findViewById(R.id.imageView);
+        User userObj = new User("MAD " + getIntent().getStringExtra("Random"),
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+                1, false);
+        nameText.setText(userObj.name);
+        descriptionText.setText(userObj.description);
+        messageButton.setText("Message");
+        if (userObj.followed) {
+            followButton.setText("Unfollow");
+        } else {
+            followButton.setText("Follow");
+        }
+
+        followButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v(TAG, "Follow Button Press");
+                userObj.followed = !(userObj.followed);
+                if (userObj.followed) {
+                    followButton.setText("Unfollow");
+                } else {
+                    followButton.setText("Follow");
+                }
+            }
+        });
+
+        messageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v(TAG, "Message Button Press");
+                Intent myIntent = new Intent(MainActivity.this, MessageGroup.class);
+                startActivity(myIntent);
+            }
+        });
+    }
+}
